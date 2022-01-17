95391756d2b89afd93d9e1310d34615c6851209c
aron-knowgas <aron@knowgas.com>
2022-01-16 21:13:27 -0600

    generate sudoku two-dimensional array

--- sudoku.js
+++ sudoku.js
@@ -1,1 +1,16 @@
+//hello world test
 document.getElementById("app").innerHTML = "hello, sudoku";
+
+// generate main table 
+var mainTable = [];
+
+// create 9 rows with 9 cells in each
+for (var x  = 0; x < 9; x++){
+    var row = [];
+
+    for (var y = 0; y < 9; y++){
+        row[y] = (x * 9) + (y + 1);
+    }
+
+    mainTable[x] = row;
+}
